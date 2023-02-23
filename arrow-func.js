341f5e4cd3446ffffb003e6e5ca237d1c7bc40f1
g3vind <70854788+g3vind@users.noreply.github.com>
2023-02-23 20:04:27 +0530

    'commit'

---
+++ arrow-func.js
@@ -0,0 +1,9 @@
+// const area = (radius) => {
+//   return 3.13 * radius ** 2;
+// };
+// console.log(area(7));
+
+const area = (radius) => {
+  return 3.14 * radius ** 2;
+};
+area;
---
+++ func.js
@@ -0,0 +1,21 @@
+// const calcArea = function (radius) {
+//   area = 3.14 * radius ** 2;
+//   return console.log(area);
+// };
+
+// calcArea(7);
+
+// const calcArea = (radius) => 3.14 * radius ** 2;
+// console.log(calcArea(7));
+
+// const greet = () => "hello";
+// console.log(greet());
+
+const bill = (products, tax) => total;
+let total = 0;
+let products = 0;
+for (let i = 0; i < products.length; i++) {
+  total += products[i] + products[i] * tax;
+}
+
+console.log(bill(1000, 10));
