02e75d421c46276b4390cd50db40a6a5f56e19c1
g3vind <70854788+g3vind@users.noreply.github.com>
2023-02-25 00:13:10 +0530

    '25-02'

---
+++ primitive-and-reference-types.js
@@ -0,0 +1,6 @@
+// primitive types
+let scoreOne = 50;
+let scoreTwo = scoreOne;
+console.log(`scoreOne : ${scoreOne} ,scoreTwo : ${scoreTwo}`);
+scoreOne = 100;
+console.log(`scoreOne : ${scoreOne} ,scoreTwo : ${scoreTwo}`);
