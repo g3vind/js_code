8f295b34cc2faf4686bf3e1349256f73a5c374ff
g3vind <70854788+g3vind@users.noreply.github.com>
2023-02-25 23:07:06 +0530

    '25-02'

---
+++ promise.js
@@ -0,0 +1,18 @@
+const getTodos = (resource, callback) => {
+  const request = new XMLHttpRequest();
+
+  request.addEventListener("readystatechange", () => {
+    if (request.readyState === 4 && request.status === 200) {
+      const data = JSON.parse(request.responseText);
+      callback(undefined, data);
+      console.log(request, request.responseText);
+    } else if (request.readyState === 4) {
+      console.log("could not fetch data", undefined);
+    }
+  });
+
+  //   request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
+  request.open("GET", resource);
+
+  request.send();
+};
